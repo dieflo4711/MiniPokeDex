5885cc8afe5e91468601aea35febe4d73cb4ca09
diego flores leon <diegoleon4711@hotmail.com>
2023-05-24 15:19:38 +0200

    Created a side menu view

---
+++ MiniPokedex/MiniPokedex/Views/SideMenuView.swift
@@ -0,0 +1,45 @@
+//
+//  SideMenuView.swift
+//  MiniPokedex
+//
+//  Created by diego flores leon on 2023-05-24.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Pokedex")
+                .bold()
+                .font(.title)
+            IconTextRow(systemName: "bookmark", text: "Bookmarked")
+                .foregroundColor(.blue)
+            IconTextRow(systemName: "checkmark.circle", text: "Selected")
+                .foregroundColor(.blue)
+            
+            Spacer()
+        }
+        .padding(.top, 32)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(white: 0.9))
+    }
+}
+
+struct IconTextRow: View {
+    let systemName: String
+    let text: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: systemName)
+            Text(text)
+        }
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
